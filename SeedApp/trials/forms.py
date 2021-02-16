f95608d8442d08888cfcb59787da7242d4671947
Luther <chikumbaluther@gmail.com>
2021-02-16 16:28:01 -0700

    Update me.

--- SeedApp/trials/forms.py
+++ SeedApp/trials/forms.py
@@ -11,38 +11,26 @@ class Trial_Form(forms.ModelForm):
 
     class Meta:
         model = Trial
-        fields = ('crop',)
-        fields = ('latitude',)
-        fields = ('longitude',)
-        fields = ('date',)
-        fields = ('notes',)
-        fields = ('user',)
+        fields = ['crop', 'latitude', 'longitude', 'date', 'notes', 'user']
 
 class Products_Form(forms.ModelForm):
-    product = forms.CharField(label='product', max_length=100)
+    product = forms.CharField(error_messages={'required':'Please enter your products'})
     timing = forms.CharField(error_messages={'required':'Please enter your timing'})
     rate = forms.FloatField(error_messages={'required':'Please enter your date'})
     rate_unit = forms.CharField(error_messages={'required':'Please enter your value'})
-    # treatment_id = forms.CharField(error_messages={'required':'Please enter your username'})
+    treatment_id = forms.CharField(error_messages={'required':'Please enter your username'})
+    
     class Meta:
         model = Product
-        fields = ('product',)
-        fields = ('timing',)
-        fields = ('rate',)
-        fields = ('rate_unit',)
-        # fields = ('treatment_id',)
+        fields = ['product', 'timing', 'rate', 'rate_unit', 'treatment_id']
 
 class Measurements_Form(forms.ModelForm):
     measure = forms.CharField(error_messages={'required':'Please enter your product'})
     unit = forms.CharField(error_messages={'required':'Please enter your timing'})
-    timimg = forms.CharField(error_messages={'required':'Please enter your date'})
+    timing = forms.CharField(error_messages={'required':'Please enter your date'})
     value = forms.FloatField(error_messages={'required':'Please enter your value'})
-    # treatment_id = forms.CharField(error_messages={'required':'Please enter your username'})
+    treatment_id = forms.CharField(error_messages={'required':'Please enter your username'})
 
     class Meta:
         model = Measure
-        fields = ('measure',)
-        fields = ('unit',)
-        fields = ('timing',)
-        fields = ('value',)
-        # fields = ('type',)
+        fields = ['measure','unit', 'timing', 'value']
