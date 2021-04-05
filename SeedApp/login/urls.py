b59557b89f07c4176bb44b72a9efd9f6edb47e4a
Carlos Olivas <carlos.olivas@nutrien.com>
2021-04-05 13:23:07 -0600

    added the login and logout views

--- SeedApp/login/urls.py
+++ SeedApp/login/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('login/', views.login),
-    
+    path('', auth_views.LoginView.as_view(template_name='login/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='login/logout.html'), name='logout'),
 ]
--- SeedApp/trials/urls.py
+++ SeedApp/trials/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home, name = 'dashboard'),
+    path('home/', views.home, name = 'dashboard'),
     path('trials/', views.trials, name = "trial_name"),
     path('<int:pk>/measurements/', views.measurements, name = "measurement_name"),
     path('<int:pk>/products/', views.products, name = "product_name"),
-    
 ]
